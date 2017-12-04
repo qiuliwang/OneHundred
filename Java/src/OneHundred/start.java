0553fea66abf2c06c2582e74df97ff78c1cdd182
WangQL <418674878@qq.com>
2017-12-04 19:45:11 +0800

    failed

---
+++ Java/src/OneHundred/start.java
@@ -0,0 +1,50 @@
+package OneHundred;
+
+import java.awt.Color;
+import java.awt.*;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+ 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+ 
+public class start extends JFrame{
+ 
+    public start(){
+        this.setSize(400,300);
+        this.setLocationRelativeTo(null);//设置居中
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         
+        JPanel jp =new JPanel();
+        jp.setLayout(new GridLayout(2,2));
+        final JButton jb1 = new JButton("弹出蓝色");
+        JButton jb2 = new JButton("弹出青色");
+        JLabel jl = new JLabel("helloxxxxxxxxxxxxxxxxxxxxxxxx");
+        jl.setBounds(20, 20, 400, 50);
+        jp.add(jl);
+        jp.add(jb1);
+        jp.add(jb2);
+        
+        //给第一个按钮添加监听
+        jb1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               jl.setText("first");
+            }
+        });
+        //给第二个按钮添加监听
+        jb2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                jl.setText("second");
+
+            }
+        });
+        this.add(jp);
+
+    }
+    public static void main(String arg[]){     
+        new start().setVisible(true);
+    }
+}  
