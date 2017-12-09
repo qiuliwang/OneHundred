eed7cb6574cc56bcaf7ec98726ac2166c0a0841e
WangQL <418674878@qq.com>
2017-12-09 15:02:16 +0800

    final version

--- WinForm/ConsoleApplication2/ConsoleApplication2/Program.cs
+++ WinForm/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -11,6 +11,19 @@ namespace ConsoleApplication2
     class Program
     {
 
+        static string getinput()
+        {
+            string str="";
+
+            str = System.Console.ReadLine();
+            while(str != "y" && str != "yes" && str != "n" && str != "no")
+            {
+                System.Console.WriteLine("*** y(yes) 或者 n(no)");
+                str = System.Console.ReadLine();
+            }
+            return str;
+        }
+
         static void Main(string[] args)
         {
             System.Console.WriteLine("*** 所有的问题都可以通过y(yes)和n(no)来回答。");
@@ -18,7 +31,7 @@ namespace ConsoleApplication2
 
             string str;
             System.Console.WriteLine("*** 你好，请问你是陶自玲嘛？");
-            str = Console.ReadLine();
+            str = getinput();
             if (str == "n" || str == "no")
             {
                 Thread.Sleep(1000);
@@ -31,20 +44,20 @@ namespace ConsoleApplication2
                 Thread.Sleep(1000);
 
                 System.Console.WriteLine("*** 你好啊，有一个叫王秋里的人想和你说几句话，可以吗？");
-                str = Console.ReadLine();
+                str = getinput();
                 if (str == "n" || str == "no")
                 {
                     Thread.Sleep(1000);
 
                     System.Console.WriteLine("*** 你确定吗？他在等你哦。");
-                    str = Console.ReadLine();
+                    str = getinput();
                     if (str == "y" || str == "yes")
                     {
                         System.Console.WriteLine("*** 求你了...他真的非常想和你讲话...");
                         Thread.Sleep(1000);
 
                         System.Console.WriteLine("*** 给他一个机会嘛?");
-                        str = Console.ReadLine();
+                        str = getinput();
                         if (str == "n" || str == "no")
                         {
                             Thread.Sleep(1000);
@@ -71,13 +84,13 @@ namespace ConsoleApplication2
                 Thread.Sleep(1000);
                 System.Console.WriteLine("你好啊桃子~");
                 Thread.Sleep(4000);
-                System.Console.WriteLine("100天可以很短，因为从开始到现在，感觉好像做了一个长梦。");
+                System.Console.WriteLine("100天可以很短，因为从开始到现在，感觉好像只是做了一个长梦。");
                 Thread.Sleep(5000);
                 System.Console.WriteLine("100天也可以很长，因为看100天之前的自己，好像完全是在看另外一个人。");
                 Thread.Sleep(6000);
                 System.Console.WriteLine("你说，100天的时候，我们像平常一样过就好。但是我怎么可能做到呢？\n从来都没有哪个100天能让我这么开心。");
                 Thread.Sleep(6000);
-                System.Console.WriteLine("当然，我也希望在将来的某一天，我们在回顾这一天的时候\n会觉得这一天只不过是我们漫长经历中平凡而又幸福的一天。");
+                System.Console.WriteLine("当然，我也希望在将来的某一天，我们在回顾这一天的时候，\n会觉得这一天只不过是我们漫长经历中平凡而又幸福的一天。");
                 Thread.Sleep(6000);
                 System.Console.WriteLine("期待第二个100天，以及更多的100天。");
                 Thread.Sleep(4000);
@@ -92,16 +105,16 @@ namespace ConsoleApplication2
 
                 System.Console.WriteLine("*** 啊，他居然走了。他个直男。他居然没有说\"我爱你\"。");
                 Thread.Sleep(4000);
-                System.Console.WriteLine("*** 但是我知道他啊，不是他不想说，是他太开心了，高兴过度了不知道该说啥。"); ;
+                System.Console.WriteLine("*** 但是我知道他啊，不是他不想说，是他紧张了。"); ;
                 Thread.Sleep(4000);
                 System.Console.WriteLine("*** 他肯定是非常爱你的了。放心吧。"); ;
                 Thread.Sleep(4000);
                 System.Console.WriteLine("*** 他试了C#、Java，结果他发现，自己已经笨到连Java都不会了。"); ;
                 Thread.Sleep(4000);
-                System.Console.WriteLine("*** 他差点把键盘砸了。所以他只能用最简单的C++来构建我了。"); ;
+                System.Console.WriteLine("*** 他差点把键盘砸了。所以他只能用最简单的方式来构建我了。"); ;
                 Thread.Sleep(4000);
                 System.Console.WriteLine("*** 所以，你觉得我怎么样？喜欢我吗？");
-                str = Console.ReadLine();
+                str = getinput();
                 Thread.Sleep(1000);
                 if (str == "n" || str == "no")
                 {
@@ -115,7 +128,7 @@ namespace ConsoleApplication2
                 System.Console.WriteLine("*** 啊，我不应该浪费你的时间。"); ;
                 Thread.Sleep(4000);
                 System.Console.WriteLine("*** 你有什么想和他说的吗？");
-                str = Console.ReadLine();
+                str = getinput();
                 if (str == "n" || str == "no")
                 {
                     Thread.Sleep(1000);
@@ -136,7 +149,10 @@ namespace ConsoleApplication2
                 }
                 Thread.Sleep(4000);
                 System.Console.WriteLine("*** 很高兴认识你。拜拜。"); ;
-                Thread.Sleep(10000);
+                string temp;
+                System.Console.WriteLine("*** 输入任何字符结束程序。");
+                temp = System.Console.ReadLine();
+
             }
 
             return;
